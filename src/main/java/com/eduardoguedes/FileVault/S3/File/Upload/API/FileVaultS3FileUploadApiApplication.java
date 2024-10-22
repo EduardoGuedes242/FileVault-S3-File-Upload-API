2ef6aacbc213b44fcacf729cf8af2afe0e79492b
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-10-22 19:47:19 -0300

    First Commit

---
+++ src/main/java/com/eduardoguedes/FileVault/S3/File/Upload/API/FileVaultS3FileUploadApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.FileVault.S3.File.Upload.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileVaultS3FileUploadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileVaultS3FileUploadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoguedes/FileVault/S3/File/Upload/API/FileVaultS3FileUploadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.FileVault.S3.File.Upload.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileVaultS3FileUploadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
