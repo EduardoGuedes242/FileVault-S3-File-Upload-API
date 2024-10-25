b2013cc3e215acdddb9210f65fb52960ee3733f9
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-10-25 01:50:50 -0300

    implementação do SWEGGER na aplicação

--- src/main/java/com/eduardoguedes/FileVault/S3/File/Upload/API/FileUploadController.java
+++ src/main/java/com/eduardoguedes/FileVault/S3/File/Upload/API/FileUploadController.java
@@ -1,5 +1,9 @@
 package com.eduardoguedes.FileVault.S3.File.Upload.API;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
@@ -23,6 +27,7 @@ public class FileUploadController {
     this.storageRepository = storageRepository;
   }
 
+  @Operation(summary = "Listar todos os arquivos da pasta", method = "GET")
   @GetMapping("/list/{path}")
   public ResponseEntity<List<String>> listAvailableFiles(@PathVariable("path") String pathDir) {
       List<String> fileNames = storageRepository.loadAll(pathDir)
@@ -32,6 +37,7 @@ public class FileUploadController {
       return ResponseEntity.ok().body(fileNames);
   }
 
+  @Operation(summary = "Download do arquivo", method = "GET")
   @GetMapping("/download/{path}/{fileName:.+}")
   public ResponseEntity<Resource> downloadFile(
            @PathVariable("path") String path,
@@ -51,20 +57,29 @@ public class FileUploadController {
             .body(resource);
   }
 
-  @PostMapping("/upload")
+  @Operation(summary = "Upload de arquivos", method = "POST")
+  @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "File Import")})
+  @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<String> handleFileUpload(
-           @RequestParam("path") String path,
-           @RequestParam("file") MultipartFile file) {
+          @Parameter(description = "Nome do diretório onde o arquivo será armazenado", required = true)
+          @RequestParam("path") String path,
+
+          @Parameter(description = "Arquivo a ser enviado, essa informação tem que ser passado como paratro igual o *PATH*", required = true)
+          @RequestParam("file") MultipartFile file) {
 
     storageRepository.store(path, file);
 
     return new ResponseEntity<String>("File Import", HttpStatus.CREATED);
   }
 
+  @Operation(summary = "Criando novo diretório", method = "POST")
+  @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "New dir create with success")})
   @PostMapping("/new-dir")
-  public ResponseEntity<String> createNewDir(@RequestParam("path") String nameDir) {
+  public ResponseEntity<String> createNewDir(
+          @Parameter(description = "Nome do diretório onde o arquivo será armazenado", required = true)
+          @RequestParam("path") String nameDir) {
     storageRepository.newDir(nameDir);
-    return new ResponseEntity<String>("new dir create with success", HttpStatus.CREATED);
+    return new ResponseEntity<String>("New dir create with success", HttpStatus.CREATED);
   }
 
 }
--- src/main/java/com/eduardoguedes/FileVault/S3/File/Upload/API/FileVaultS3FileUploadApiApplication.java
+++ src/main/java/com/eduardoguedes/FileVault/S3/File/Upload/API/FileVaultS3FileUploadApiApplication.java
@@ -1,12 +1,15 @@
 package com.eduardoguedes.FileVault.S3.File.Upload.API;
 
 import com.eduardoguedes.FileVault.S3.File.Upload.API.infra.properties.StorageProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@OpenAPIDefinition(info = @Info(title = "Upload e Download de Arquivos - EC2 AWS", version = "1", description = "Esta é uma aplicação desenvolvida em **Java** com **Spring Boot**, utilizando a infraestrutura de **EC2 da AWS**. A aplicação permite o **upload e download** de arquivos de forma simples."))
 public class FileVaultS3FileUploadApiApplication {
 
 	public static void main(String[] args) {
